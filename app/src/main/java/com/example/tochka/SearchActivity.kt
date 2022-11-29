acf1c0a251f56e76b8bd3b7dbf685034ea7dfb42
Георгий Александрович Мичкасов <michkasov.ga1668@oksei.onmicrosoft.com>
2022-11-29 11:22:37 +0500

    Main branch

--- app/src/main/java/com/example/tochka/SearchActivity.kt
+++ app/src/main/java/com/example/tochka/SearchActivity.kt
@@ -2,8 +2,11 @@ package com.example.tochka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
